4a41fcbf72db5ef8f2576ae7bf6a0a7e9a28ac7c
Krishnakumar Diwakar <krishnakumarmd@gmail.com>
2026-07-21 17:13:08 +0530

    test 4

--- bmlt/s30forZ.js
+++ bmlt/s30forZ.js
@@ -1,6 +1,6 @@
 (function () {
     const LOOP =
-        parseInt(prompt("Update every 10 seconds.\nHow many updates?", "3")) || 3;
+        parseInt(prompt("Update every 10 seconds.\nHow many updates?", "4")) || 4;
 
     let count = 0;
 
