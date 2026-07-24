7990fdd09f9d2580dad006559492bd7f2ac82e56
Krishnakumar Diwakar <krishnakumarmd@gmail.com>
2026-07-24 12:27:20 +0530

    changes 18

--- bmlt/N50forZ.js
+++ bmlt/N50forZ.js
@@ -1,6 +1,6 @@
 (function () {
     const LOOP =
-        parseInt(prompt("Update every 10 seconds.\nHow many updates?", "6")) || 6;
+        parseInt(prompt("Update every 10 seconds.\nHow many updates?", "18")) || 18;
 
     let count = 0;
 
